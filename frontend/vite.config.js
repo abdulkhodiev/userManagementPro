9b4bd000dd5bbef4423a7f5e01d0ad6bdbbd45b8
abdulkhodiev <jobirkhonabdulkhodiev4532@gmail.com>
2024-02-20 22:41:19 +0500

    error handling

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     build: {
         rollupOptions: {
-            external: ["react", "react-dom", "react-router-dom"],
+            external: [
+                "react",
+                "react-dom",
+                "react-router-dom",
+                "bootstrap/dist/css/bootstrap.min.css",
+            ],
         },
     },
 });
