84718aaa977eb0fb1a60c5a15f6197e9063ebf8a
abdulkhodiev <jobirkhonabdulkhodiev4532@gmail.com>
2024-02-21 11:26:48 +0500

    Cors error handling

--- backend/index.js
+++ backend/index.js
@@ -21,14 +21,7 @@ mongoose
 
 app.use(express.json());
 
-app.use(function (request, response, next) {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
+app.use(cors({ origin: "*" }));
 
 app.use("/api/user", authRoute);
 app.use("/api/control", userRoute);
